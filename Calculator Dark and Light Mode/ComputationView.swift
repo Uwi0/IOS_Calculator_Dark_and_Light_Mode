63e51f7e54917a3f76dd8a2079d9883c4a715dde
uwi0 <kakapoweb@gmail.com>
2024-08-30 17:30:40 +0700

    finishing computation view

--- Calculator Dark and Light Mode/ComputationView.swift
+++ Calculator Dark and Light Mode/ComputationView.swift
@@ -6,15 +6,33 @@ struct ComputationView: View {
     let mainResult: String
     
     var body: some View {
-        VStack {
-            Text("computation view")
-            Text(currentComputation)
-            Text(mainResult)
-        }
+        VStack(spacing: 10) {
+            
+            HStack {
+                Spacer()
+                Text(currentComputation)
+                    .foregroundStyle(foregroundDigitsColor)
+                    .lineLimit(1)
+            }
+            .minimumScaleFactor(0.1)
+            
+            HStack {
+                Spacer()
+                Text(mainResult)
+                    .foregroundStyle(foregroundDigitsColor)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(1)
+            }
+            .minimumScaleFactor(0.1)
+            
+        }.padding(.horizontal)
         
     }
 }
 
 #Preview {
-    ComputationView(currentComputation: "hello", mainResult: "world")
+    VStack {
+        ComputationView(currentComputation: "1200 + 926", mainResult: "world")
+    }
 }
