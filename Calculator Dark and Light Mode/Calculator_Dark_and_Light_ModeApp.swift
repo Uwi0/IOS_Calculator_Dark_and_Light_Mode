4f562d8e4e3a066954c0825fe047f8e06dfd9d05
uwi0 <kakapoweb@gmail.com>
2024-08-30 15:59:09 +0700

    Initial Commit

---
+++ Calculator Dark and Light Mode/Calculator_Dark_and_Light_ModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_Dark_and_Light_ModeApp.swift
+//  Calculator Dark and Light Mode
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_Dark_and_Light_ModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator Dark and Light Mode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator Dark and Light Mode
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
